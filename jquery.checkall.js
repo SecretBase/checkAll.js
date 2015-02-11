fd7c5c2ef82cf480c77cc82523ddabf589844408
SecondHong <whpigpig@gmail.com>
2015-02-11 21:48:25 +0800

    optimize a bit of the selector

--- jquery.checkall.js
+++ jquery.checkall.js
@@ -41,7 +41,7 @@
                     return;
                 }
 
-                if ($scope.find('input[type="checkbox"]:checked').not($master_checkbox).length == $scope.find('input[type="checkbox"]').not($master_checkbox).length)
+                if ($scope.find('input[type="checkbox"]:checked').not($master_checkbox).not(':checked').length == 0)
                     $master_checkbox.prop('checked', true);
             });
 
