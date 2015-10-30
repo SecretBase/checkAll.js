840518e6265143c19aa5146df02c5b5303505151
SecondHong <whpigpig@gmail.com>
2015-10-30 21:30:26 +0800

    Clean up up coding style

--- docs/jquery.checkall.js
+++ docs/jquery.checkall.js
@@ -1,42 +1,42 @@
-;(function($)
-{
-    $.fn.checkAll = function (options)
-    {
+(function ($) {
+
+    $.fn.checkAll = function (options) {
+
         var options = $.extend({
                 scope: 'form',
                 onMasterClick: null,
                 onScopeChange: null
             }, options);
 
-        return this.each(function() {
+        return this.each(function () {
 
             var $master_checkbox = $(this),
                 $scope = options.scope instanceof jQuery ? options.scope : $master_checkbox.closest(options.scope);
 
-            $master_checkbox.on('click', function(e) {
+            $master_checkbox.on('click', function (event) {
 
-                if ($master_checkbox.is(':checked'))
-                    $scope.find('input[type="checkbox"]').not($master_checkbox).prop('checked', true).trigger('change');
-                else
-                    $scope.find('input[type="checkbox"]').not($master_checkbox).prop('checked', false).trigger('change');
+                $scope.find('input[type="checkbox"]').not($master_checkbox).prop('checked', $master_checkbox.is(':checked')).trigger('change');
 
-                if (typeof options.onMasterClick === 'function')
-                        options.onMasterClick($master_checkbox, $scope);
+                if (typeof options.onMasterClick === 'function') {
+                    options.onMasterClick($master_checkbox, $scope);
+                }
             });
 
-            $scope.on('change', 'input[type="checkbox"]', function(e) {
+            $scope.on('change', 'input[type="checkbox"]', function (event) {
 
                 var $changed_checkbox = $(this);
 
-                if ($changed_checkbox.is($master_checkbox))
+                if ($changed_checkbox.is($master_checkbox)) {
                     return;
-
-                if (typeof options.onScopeChange === 'function')
-                        options.onScopeChange($master_checkbox, $changed_checkbox, $scope);
+                }
 
                 if ( ! $changed_checkbox.is(':checked')) {
                     $master_checkbox.prop('checked', false);
                     return;
+                }
+
+                if (typeof options.onScopeChange === 'function') {
+                    options.onScopeChange($master_checkbox, $changed_checkbox, $scope);
                 }
 
                 if ($scope.find('input[type="checkbox"]').not($master_checkbox).not(':checked').length === 0)
